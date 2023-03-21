be09ff3d054b59b7665d4097b1f2b216baafc72b
Giga <gigabyte5671@gmail.com>
2023-03-21 16:58:22 +1300

    Add getter & setter for the `CSSEditor`'s stylesheet

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
 import { createApp } from 'vue';
-import Editor from './Editor.vue';
-
-import './assets/base.css';
+import type { ComponentPublicInstance } from 'vue';
+import Editor from '@/Editor.vue';
+import '@/assets/base.css';
+import type { StyleRule } from '@/types';
 
 export class CSSEditor {
 	private app;
 	public editor;
+	private internalStylesheet = [] as StyleRule[];
 
 	constructor (element: string) {
 		this.app = createApp(Editor);
-		this.editor = this.app.mount(element);
-		Editor.watch
+		this.editor = this.app.mount(element) as ComponentPublicInstance<{}, {}, { stylesheet: StyleRule[] }>;
+	}
+
+	get stylesheet (): StyleRule[] {
+		return this.internalStylesheet;
+	}
+
+	set stylesheet (value: StyleRule[]) {
+		this.internalStylesheet = value;
 	}
 }
