03dd57c558c7e01443370936a458674c7a83747d
Giga <gigabyte5671@gmail.com>
2023-03-23 13:20:57 +1300

    Build as library for production, demo for development

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,26 @@
+import { resolve } from 'path';
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
+import type { BuildOptions } from 'vite';
 import vue from '@vitejs/plugin-vue';
+
+const devBuild: BuildOptions = {
+	sourcemap: true
+};
+
+const prodBuild: BuildOptions = {
+	sourcemap: true,
+	lib: {
+		entry: resolve(__dirname, 'src/index.ts'),
+		name: 'CSSEditor',
+		fileName: 'index',
+	},
+	rollupOptions: {
+		output: {
+        	chunkFileNames: '[name].js'
+		}
+    }
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +29,6 @@ export default defineConfig({
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url))
 		}
-	}
+	},
+	build: process.env.NODE_ENV === "production" ? prodBuild : devBuild
 });
